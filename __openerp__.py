6bb6e4aa6b27cd51ef7c6fdc9c63bb817954ec26
Zoe <jyun55520@gmail.com>
2016-09-07 15:44:41 +0800

    1.0.9.5.6

---
+++ __openerp__.py
@@ -0,0 +1,24 @@
+{
+    'name': "Setting",
+    'version': '1.0',
+    'depends': [],
+    'author': "alltop",
+    'category': '',
+    'description': """
+
+    """,
+    'depends': [
+        'base_setup',
+    ],
+    'data': [
+                "views/setting_aid_company.xml",
+                "views/menu.xml",
+                "views/setting_aid_system.xml",
+                "views/setting_aid_tax.xml",
+                "views/setting_aid_supply.xml",
+                "views/setting_aid_relate.xml",
+                "views/setting_aid_shareclass.xml"
+            ],
+    'demo': [],
+    'application': True,
+}
