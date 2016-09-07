0556b4af9646208360f7f71def690e3ea0909165
Zoe <jyun55520@gmail.com>
2016-09-07 10:06:48 +0800

    1.0.9.7.1

--- __openerp__.py
+++ __openerp__.py
@@ -10,7 +10,10 @@
     'data': [
                 "views/setting_aid_company.xml",
                 "views/menu.xml",
-                "views/setting_aid_system.xml"
+                "views/setting_aid_system.xml",
+                "views/setting_aid_tax.xml",
+                "views/setting_aid_supply.xml",
+                "views/setting_aid_relate.xml"
             ],
     'demo': [],
     'application': True,
